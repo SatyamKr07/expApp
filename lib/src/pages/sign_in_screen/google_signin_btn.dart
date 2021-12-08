9e1c21b62fa274811e87710e694ecf3da494bd98
Satyam <satyamismyname@gmail.com>
2021-12-08 19:28:41 +0530

    ui improvements

--- lib/src/pages/sign_in_screen/google_signin_btn.dart
+++ lib/src/pages/sign_in_screen/google_signin_btn.dart
@@ -17,7 +17,9 @@ class _GoogleSignInButtonState extends State<GoogleSignInButton> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 16.0),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 16.0,
+      ),
       child: _isSigningIn
           ? Center(
               child: const CircularProgressIndicator(
