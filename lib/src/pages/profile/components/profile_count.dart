544581171d20bf78ae92a320a1e67cf52e8a1a6e
Satyam <satyamismyname@gmail.com>
2021-12-08 14:28:24 +0530

    profile page count fetched

--- lib/src/pages/profile/components/profile_count.dart
+++ lib/src/pages/profile/components/profile_count.dart
@@ -1,19 +1,21 @@
+import 'package:commentor/src/controllers/user_controller.dart';
 import 'package:commentor/src/models/user_model.dart';
 import 'package:flutter/material.dart';
+import 'package:get/instance_manager.dart';
 
 class ProfileCount extends StatelessWidget {
   UserModel userModel;
   ProfileCount({Key? key, required this.userModel}) : super(key: key);
-
+  final userController = Get.find<UserController>();
   @override
   Widget build(BuildContext context) {
-    _profileComponent({required double count, required String title}) {
+    _profileComponent({required int count, required String title}) {
       return Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text(
-            "$count k",
+            "$count ",
             // style: KCustomTextstyle.kBold(context, 12),
           ),
           Text(
@@ -27,10 +29,22 @@ class ProfileCount extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        _profileComponent(count: 0, title: "Followers"),
-        _profileComponent(count: 0, title: "Following"),
-        _profileComponent(count: 0, title: "Comments"),
-        _profileComponent(count: 0, title: "Likes"),
+        _profileComponent(
+          count: userController.appUser.followersCount,
+          title: "Followers",
+        ),
+        _profileComponent(
+          count: userController.appUser.followingCount,
+          title: "Following",
+        ),
+        _profileComponent(
+          count: userController.appUser.totalCommentsCount,
+          title: "Comments",
+        ),
+        _profileComponent(
+          count: userController.appUser.totalLikesCount,
+          title: "Likes",
+        ),
       ],
     );
   }
